cd9e6753dbedf49095ccff0a8b7308c186f18d4e
Erick <erick_aug@hotmail.com>
2017-10-26 21:06:32 -0200

    Main Dijkstra

---
+++ src/dijkstra/Dijkstra.java
@@ -0,0 +1,18 @@
+
+package dijkstra;
+
+public class Dijkstra {
+
+    public static void main(String[] args) {
+        //Leitura do teclado
+        GrafoPonderado g = new GrafoPonderado();
+        g.leDoTeclado();
+        
+        int n = g.totalDeVertices;
+        int distancias[][] = new int[n][n];
+        int[] vet = new int[n];
+        int i, j;
+        
+        
+    }
+}
