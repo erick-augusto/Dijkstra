37e662d05ac6be57db6162135e8a1ddb6364277e
Erick <erick_aug@hotmail.com>
2017-10-26 21:07:05 -0200

    Algoritmo

---
+++ src/dijkstra/Dij.java
@@ -0,0 +1,16 @@
+
+package dijkstra;
+
+public class Dij {
+    
+    public Dij(){
+        
+    }
+    
+    public Vertice[] vertices;
+    private int[] vet;
+    
+    public void Dijkstra(Grafo g, Vertice s){
+        
+    }
+}
